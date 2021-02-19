5c47da5ee9c5387b9344d6e0a464807b832f8f07
Adao Ribeiro <sarinjr1024@hotmail.com>
2021-02-18 23:47:51 -0400

    Outro Commit

--- Program.cs
+++ Program.cs
@@ -15,8 +15,7 @@ namespace MoverArquivosE
         static void Main(string[] args)
         {
             string arquivos = @"C:\Users\jr\Documents\CARGA_TENS";
-            string saida = @"C:\Users\jr\Documents\carga_itensnovo";
-
+            string saida = @"C:\Users\jr\Documents\TESTE";
 
             string[] files = Directory.GetFiles(arquivos);
 
